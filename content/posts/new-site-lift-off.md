77798d034b1fd42a6c402125976654828ef4225d
Ryan Lynch <ryanlynch717@gmail.com>
2023-10-06 14:22:08 +0100

    first commit site setup

---
+++ content/posts/new-site-lift-off.md
@@ -0,0 +1,9 @@
+---
+title: YAY! New site has lift-off
+---
+
+Hey! Welcome back, we're pleased to announce that our new site has now launched!
+
+```javascript
+<p>Hello world!</p>
+```
---
+++ tina/config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "author",
+        label: "Authors",
+        path: "content/authors",
+        fields: [
+          {
+            type: "string",
+            name: "name",
+            label: "Name",
+            isTitle: true,
+            required: true,
+          },
+        ],
+      },
+    ],
+  },
+});
